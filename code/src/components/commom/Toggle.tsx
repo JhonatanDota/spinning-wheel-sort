f769840b3470902684e58c85d082b392c7309827
Jhonatan de Azevedo da Silva <jhonatanazeved@hotmail.com>
2025-07-15 18:40:55 -0300

    adicionando componente de toggle

---
+++ code/src/components/commom/Toggle.tsx
@@ -0,0 +1,20 @@
+interface ToggleProps {
+  checked: boolean;
+  setChecked: (checked: boolean) => void;
+}
+
+export default function Toggle(props: ToggleProps) {
+  const { checked, setChecked } = props;
+
+  return (
+    <label className="cursor-pointer">
+      <input
+        className="sr-only peer"
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+      />
+      <div className="relative w-11 h-6 bg-gray-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+    </label>
+  );
+}
